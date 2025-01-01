bc516e3a79ca0a84209dc37409ccad6ac67856d8
arnavsri454 <arnavsri454@gmail.com>
2025-01-01 18:13:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ function sendWish() {
         messageElement.textContent = "Happy New Year, Didi! 🎉 This New Year, I wish your dreams come true and all your efforts finally give you the results you deserve. from your younger brother Arnav!(SUMMONER),  🎊";
     } else if (name === "yartharth" || name === "ansh" || name === "sahil") {
         messageElement.textContent = "Happy New Year, Bhaiya! 🎉 ,Here's to a new year filled with fresh starts, new adventures, and memories to treasure forever. Wishing you a happy, prosperous 2025! once Again Happy new year bhaiya from Arnav ( SUMMONER) ! 🎊";
-    } else if (name === "anvesha" || name === "Shreyansh") {
+    } else if (name === "anvesha" || name === "shreyansh") {
         messageElement.textContent = "Happy New Year, Dear friend! 🎉 , Here's to another year of endless love, joy, and adventures with you. Happy New Year, Wishing you a wonderful year ahead, from Arnav! 🎊";
-    }else if (name === "kanishka" ) {
+    }else if (name === "kanishka"  || name === "anusha" ) {
         messageElement.textContent = "To my little sister, Kanishka, may the New Year bless you with endless smiles, laughter, and wonderful memories. 💖🎉 Wishing you a fantastic year ahead!, Happy new year ahead , from your brother Arnav(SUMMONER)";
     }
      else {
